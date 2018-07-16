f4d5ee994030b8f5fd46174e5fad5640a3723004
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-16 18:07:29 +0700

    "Gson"

--- app/src/main/java/com/ptp/phamtanphat/quanlynhahangretrofit2/Service/RetrofitInit.java
+++ app/src/main/java/com/ptp/phamtanphat/quanlynhahangretrofit2/Service/RetrofitInit.java
@@ -1,5 +1,8 @@
 package com.ptp.phamtanphat.quanlynhahangretrofit2.Service;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
@@ -8,15 +11,18 @@ import retrofit2.Retrofit;
 public class RetrofitInit {
     public static Retrofit retrofit = null;
 
+    //Phương thức cấu hình retrofit
     public static Retrofit getRetrofit(String base_url){
+
+        //Cấu hình những tương tác về internet
         OkHttpClient builder = new OkHttpClient.Builder()
                                 .readTimeout(10, TimeUnit.SECONDS)
                                 .writeTimeout(10,TimeUnit.SECONDS)
                                 .connectTimeout(10,TimeUnit.SECONDS)
                                 .retryOnConnectionFailure(true)
                                 .build();
-
-
+        //Bộ chuyển đổi gson
+        Gson gson = new GsonBuilder().setLenient().create();
 
     }
 }
