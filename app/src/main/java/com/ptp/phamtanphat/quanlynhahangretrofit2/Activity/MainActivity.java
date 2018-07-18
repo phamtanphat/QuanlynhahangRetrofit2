9446ce4076e693c559488ef0ee2228f473f3c304
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-18 19:01:09 +0700

    "Delete"

--- app/src/main/java/com/ptp/phamtanphat/quanlynhahangretrofit2/Activity/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/quanlynhahangretrofit2/Activity/MainActivity.java
@@ -2,17 +2,18 @@ package com.ptp.phamtanphat.quanlynhahangretrofit2.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.ptp.phamtanphat.quanlynhahangretrofit2.Adapter.MonanAdapter;
-import com.ptp.phamtanphat.quanlynhahangretrofit2.ListenData;
 import com.ptp.phamtanphat.quanlynhahangretrofit2.Model.Monan;
 import com.ptp.phamtanphat.quanlynhahangretrofit2.R;
 import com.ptp.phamtanphat.quanlynhahangretrofit2.Service.RetrofitAPI;
 import com.ptp.phamtanphat.quanlynhahangretrofit2.Service.RetrofitData;
+import com.ptp.phamtanphat.quanlynhahangretrofit2.Service.RetrofitInit;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,12 +21,12 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements ListenData{
+public class MainActivity extends AppCompatActivity{
 
     ListView lvMonan;
     MonanAdapter monanAdapter;
     ArrayList<Monan> mangmonan;
-    ListenData listenData = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,15 +34,14 @@ public class MainActivity extends AppCompatActivity implements ListenData{
 
         lvMonan = findViewById(R.id.listview);
 
-        listenData = MainActivity.this;
-
         RetrofitAPI retrofitAPI = RetrofitData.getData();
         Call<List<Monan>> callback = retrofitAPI.getMonan();
         callback.enqueue(new Callback<List<Monan>>() {
             @Override
             public void onResponse(Call<List<Monan>> call, Response<List<Monan>> resposne){
                 mangmonan = (ArrayList<Monan>) resposne.body();
-                listenData.DataOnResponse(mangmonan);
+                monanAdapter = new MonanAdapter(MainActivity.this,android.R.layout.simple_list_item_1,mangmonan);
+                lvMonan.setAdapter(monanAdapter);
             }
 
             @Override
@@ -49,16 +49,25 @@ public class MainActivity extends AppCompatActivity implements ListenData{
 
             }
         });
-    }
+        lvMonan.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                RetrofitAPI retrofitAPI1 = RetrofitData.getData();
+                Call<String> callback = retrofitAPI1.ondelete(mangmonan.get(position).getId());
+                callback.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        String ketqua = (response.body().equals("Success")) ? "Thanh cong" : "That bai";
+                        Toast.makeText(MainActivity.this, ketqua, Toast.LENGTH_SHORT).show();
+                    }
 
-    @Override
-    public void DataOnResponse(List<Monan> Mangmonan) {
-        monanAdapter = new MonanAdapter(MainActivity.this,android.R.layout.simple_list_item_1,Mangmonan);
-        lvMonan.setAdapter(monanAdapter);
-    }
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
 
-    @Override
-    public void Fail(Throwable t) {
-
+                    }
+                });
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/ptp/phamtanphat/quanlynhahangretrofit2/Service/RetrofitAPI.java
+++ app/src/main/java/com/ptp/phamtanphat/quanlynhahangretrofit2/Service/RetrofitAPI.java
@@ -5,7 +5,12 @@ import com.ptp.phamtanphat.quanlynhahangretrofit2.Model.Monan;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface RetrofitAPI {
 
@@ -13,4 +18,13 @@ public interface RetrofitAPI {
     //Tạo request method
     @GET("getdata.php")
     Call<List<Monan>> getMonan();
+//    http://localhost/Quanlynhahang/delete.php?id=1
+//    @GET("delete.php?id=&ten")
+//    Call<String> onDelete(@Query("id")String id,@Query("ten"));
+//    @GET("nhahang/{a}/hinhanh.php")
+//    Call<String> onChange(@Path());
+
+    @FormUrlEncoded
+    @POST("delete.php")
+    Call<String> ondelete(@Field("id") String id);
 }
